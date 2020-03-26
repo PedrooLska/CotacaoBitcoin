9b94389bae690fbdaa90414b16c492652fd5bbb6
PedrooLska <pedrolucas81@hotmail.com>
2020-03-26 13:37:13 -0300

    Project Finished

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <priceBTC />
+    <priceBTC title="Price BTC" />
   </div>
 </template>
 
@@ -14,3 +14,23 @@ export default {
   }
 };
 </script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Roboto&display=swap");
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: "Roboto", sans-serif;
+  background: linear-gradient(to right, rgb(54, 1, 85), rgb(93, 2, 146));
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  height: 100vh;
+}
+</style>
--- frontend/src/components/priceBTC.vue
+++ frontend/src/components/priceBTC.vue
@@ -1,6 +1,15 @@
 <template>
-  <div id="hello">
-    <h1>{{ coin }}</h1>
+  <div class="priceBTC">
+    <h1>{{ title }}</h1>
+
+    <div v-for="coins in coin" v-bind:key="coins.code">
+      <big>{{ coins.description }}:</big>
+
+      <span>
+        <span v-html="coins.symbol"></span>
+        {{ coins.rate_float | valueToFixed }}
+      </span>
+    </div>
   </div>
 </template>
 
@@ -9,6 +18,7 @@ import axios from "axios";
 
 export default {
   name: "priceBTC",
+  props: ["title"],
   data() {
     return {
       coin: null
@@ -19,6 +29,33 @@ export default {
     axios
       .get("https://api.coindesk.com/v1/bpi/currentprice.json")
       .then(response => (this.coin = response.data.bpi));
+  },
+  filters: {
+    valueToFixed(value) {
+      return value.toFixed(2);
+    }
   }
 };
 </script>
+
+<style scoped>
+.priceBTC {
+  color: white;
+  background: rgb(60, 1, 95);
+  border-radius: 30px;
+  width: 450px;
+  padding: 50px;
+  margin: 0 auto;
+}
+
+.priceBTC h1 {
+  margin: 15px;
+  text-align: center;
+}
+
+.priceBTC big {
+  font-size: 16px;
+  font-weight: bold;
+  margin-right: 10px;
+}
+</style>
